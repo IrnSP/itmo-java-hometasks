9178edd4b1d8dcfda331441019609a7a23f93d43
Irina Sokolova <irina@gmail.com>
2022-09-24 14:20:03 +0300

    Homework2

--- src/hw2/Task1.java
+++ src/hw2/Task1.java
@@ -1,4 +1,22 @@
 package hw2;
 
 public class Task1 {
+    public static void main(String[] args) {
+        //Дан номер месяца, например int month = 5. В зависимости от данного значения вывести в консоль время года
+        int month = 5;
+        if (month==1) System.out.println("Winter");
+        else if (month==2) System.out.println("Winter");
+        else if (month==12) System.out.println("Winter");
+        else if (month==3) System.out.println("Frühling");
+        else if (month==4) System.out.println("Frühling");
+        else if (month==5) System.out.println("Frühling");
+        else if (month==6) System.out.println("Sommer");
+        else if (month==7) System.out.println("Sommer");
+        else if (month==8) System.out.println("Sommer");
+        else if (month==9) System.out.println("Herbst");
+        else if (month==10) System.out.println("Herbst");
+        else if (month==11) System.out.println("Herbst");
+        else System.out.println("Fehler. Nur positive ganze Zahlen von 1 bis 12");
+
+    }
 }
--- src/hw2/Task2.java
+++ src/hw2/Task2.java
@@ -1,4 +1,36 @@
 package hw2;
 
 public class Task2 {
+    public static void main(String[] args) {
+        /* Дана переменная sum - сумма покупки и переменная saleCode - номер купона.
+        Если номер купона равен 4525 - вывести сумму с учетом скидки 30%
+        Если номер купона равен 6424 или 7012 - вывести сумму с учетом скидки 20%
+        Если номер купона равен 7647 или 9011 или 6612 - вывести сумму с учетом скидки 10%
+        В остальных случаях скидка не предусмотрена, вывесть полную стоимость покупки. */
+
+        int sum = 100, saleCode = 1234;
+        switch (saleCode) {
+            case 4525:
+                System.out.println(sum*0.7);
+                break;
+            case 6424:
+                System.out.println(sum*0.8);
+                break;
+            case 7012:
+                System.out.println(sum*0.8);
+                break;
+            case 7647:
+                System.out.println(sum*0.9);
+                break;
+            case 9011:
+                System.out.println(sum*0.9);
+                break;
+            case 6612:
+                System.out.println(sum*0.9);
+                break;
+            default:
+                System.out.println(sum);
+        }
+
+    }
 }
